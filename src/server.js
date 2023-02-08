20d1f35d9b78555ea69e64ecf59a6b89feb4a64c
kdh94kor <kdh94kor@naver.com>
2023-02-08 21:58:47 +0900

    2023-02-08

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+app.get("/",(req,res) => res.render("home"));
+app.get("/*",(req,res) => res.redirect("home")); // 홈 외 경로를 접속할때 홈으로
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+app.listen(3000, handleListen);
